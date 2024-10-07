402bb24c284d02402a9799a4c238a10de3ce9ebc
Viktoriia Shalaieva <163899438+Viktoriia-Shalaieva@users.noreply.github.com>
2024-10-07 17:53:50 +0300

    Update PostgreSQL.sql

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -29,3 +29,21 @@ SELECT ABS(
 ) AS diff_salary
 ;
 
+/*
+Finding Updated Records 
+We have a table with employees and their salaries, however, some of the records are old and contain outdated salary information. 
+Find the current salary of each employee assuming that salaries increase each year. Output their id, first name, last name, department ID, and current salary. Order your list by employee ID in ascending order.
+
+Table: ms_employee_salary
+
+id: int
+first_name: varchar
+last_name: varchar
+salary: int
+department_id: int
+*/
+
+SELECT id,	first_name,	last_name,	department_id, MAX(salary) as MAX
+FROM ms_employee_salary
+GROUP BY id, first_name,	last_name,	department_id
+ORDER BY id;
