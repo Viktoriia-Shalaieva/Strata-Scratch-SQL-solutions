35140311722a45fe17acd8ee08ed413bd797a87c
Viktoriia Shalaieva <163899438+Viktoriia-Shalaieva@users.noreply.github.com>
2024-10-07 18:04:42 +0300

    Update PostgreSQL.sql

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -47,3 +47,30 @@ SELECT id,	first_name,	last_name,	department_id, MAX(salary) as MAX
 FROM ms_employee_salary
 GROUP BY id, first_name,	last_name,	department_id
 ORDER BY id;
+
+/*
+Bikes Last Used
+Find the last time each bike was in use. Output both the bike number and the date-timestamp of the bike's last use (i.e., the date-time the bike was returned). Order the results by bikes that were most recently used.
+
+Table: dc_bikeshare_q1_2012
+
+duration: varchar
+duration_seconds: int
+start_time: datetime
+start_station: varchar
+start_terminal: int
+end_time: datetime
+end_station: varchar
+end_terminal: int
+bike_number: varchar
+rider_type: varchar
+id: int
+*/
+
+SELECT 
+    bike_number
+    , MAX(end_time) AS last_use
+FROM dc_bikeshare_q1_2012
+GROUP BY bike_number
+ORDER BY last_used DESC
+;
