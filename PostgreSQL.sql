5fd6a5b3eb57c7e12f0295408fc25a5864615bf8
Viktoriia Shalaieva <163899438+Viktoriia-Shalaieva@users.noreply.github.com>
2024-10-07 18:12:14 +0300

    Update PostgreSQL.sql

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -74,3 +74,35 @@ FROM dc_bikeshare_q1_2012
 GROUP BY bike_number
 ORDER BY last_used DESC
 ;
+
+/*
+Reviews of Hotel Arena
+Find the number of rows for each review score earned by 'Hotel Arena'. Output the hotel name (which should be 'Hotel Arena'), review score along with the corresponding number of rows with that score for the specified hotel.
+
+Table: hotel_reviews
+
+hotel_address: varchar
+additional_number_of_scoring: int
+review_date:datetime
+average_score:float
+hotel_name:varchar
+reviewer_nationality:varchar
+negative_review:varchar
+review_total_negative_word_counts:int
+total_number_of_reviews:int
+positive_review:varchar
+review_total_positive_word_counts:int
+total_number_of_reviews_reviewer_has_given:int
+reviewer_score:float
+tags:varchar
+days_since_review:varchar
+lat:float
+lng:float
+*/
+SELECT hotel_name
+    , reviewer_score
+    , COUNT(*)
+FROM hotel_reviews
+WHERE hotel_name = 'Hotel Arena'
+GROUP BY 1, 2
+;
